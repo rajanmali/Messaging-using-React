8917735233a1d85485183c035e82ea757474ab23
rajanmali <mysteriouskiller50@gmail.com>
2019-09-13 17:16:05 +0545

    Added viewing channels to messages

--- src/Components/SidePanel/Channel.js
+++ src/Components/SidePanel/Channel.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import { Menu, Icon, Modal, Form, Button, Input } from "semantic-ui-react";
+import firebase from "../../firebase";
 
 class Channel extends Component {
     state = {
+        user: this.props.currentUser,
         channels: [],
         channelName: "",
         channelDetails: "",
+        channelsRef: firebase.database().ref("channels"),
         modal: false
+    };
+
+    componentDidMount() {
+        this.addListeners();
+    }
+
+    addListeners = () => {
+        let loadedChannels = [];
+        this.state.channelsRef.on("child_added", snap => {
+            loadedChannels.push(snap.val());
+            this.setState({ channels: loadedChannels });
+        });
     };
 
     openModal = () => {
@@ -21,6 +36,56 @@ class Channel extends Component {
         this.setState({ [event.target.name]: event.target.value });
     };
 
+    addChannel = () => {
+        const { user, channelName, channelDetails, channelsRef } = this.state;
+        const key = channelsRef.push().key;
+
+        const newChannel = {
+            id: key,
+            name: channelName,
+            details: channelDetails,
+            createdBy: {
+                name: user.displayName,
+                avatar: user.photoURL
+            }
+        };
+        channelsRef
+            .child(key)
+            .update(newChannel)
+            .then(() => {
+                this.setState({
+                    channelName: "",
+                    channelDetails: ""
+                });
+                this.closeModal();
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    displayChannels = channels =>
+        channels.length > 0 &&
+        channels.map(channel => (
+            <Menu.Item
+                key={channel.id}
+                onClick={() => console.log(channel)}
+                name={channel.name}
+                style={{ opcaity: 0.7 }}
+            >
+                # {channel.name}
+            </Menu.Item>
+        ));
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.isFormValid(this.state)) {
+            this.addChannel();
+        }
+    };
+
+    isFormValid = ({ channelName, channelDetails }) => channelDetails && channelName;
+
     render() {
         const { channels, modal } = this.state;
         return (
@@ -33,12 +98,13 @@ class Channel extends Component {
                         ({channels.length}){" "}
                         <Icon name="add" onClick={this.openModal} style={{ cursor: "pointer" }} />
                     </Menu.Item>
+                    {this.displayChannels(channels)}
                 </Menu.Menu>
-                // Add channel modal
+                {/* Add channel modal */}
                 <Modal basic open={modal} onClose={this.closeModal}>
                     <Modal.Header>Add a Channel</Modal.Header>
                     <Modal.Content>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Field>
                                 <Input
                                     fluid
@@ -58,7 +124,7 @@ class Channel extends Component {
                         </Form>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button color="green" inverted>
+                        <Button color="green" inverted onClick={this.handleSubmit}>
                             <Icon name="checkmark" /> Add
                         </Button>
                         <Button color="red" inverted onClick={this.closeModal}>
--- src/Components/SidePanel/index.js
+++ src/Components/SidePanel/index.js
@@ -15,7 +15,7 @@ class SidePanel extends Component {
                 style={{ background: "#4c3c4c", fontSize: "1.2em" }}
             >
                 <UserPanel currentUser={currentUser} />
-                <Channel />
+                <Channel currentUser={currentUser} />
             </Menu>
         );
     }
