93563550ed0091bcd78703d414dd548e00bd98df
rajanmali <mysteriouskiller50@gmail.com>
2019-09-13 13:22:06 +0545

    Created log in page

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -138,7 +138,7 @@ class Register extends Component {
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="blue" textAlign="center">
+                    <Header as="h1" icon color="blue" textAlign="center">
                         <Icon name="cloud" color="blue" />
                         Register Page
                     </Header>
