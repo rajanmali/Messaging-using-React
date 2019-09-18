ff8f6781bbe572340b971e0538751ed8e6644e95
rajanmali <mysteriouskiller50@gmail.com>
2019-09-18 09:50:36 +0545

    dynamically displaying starred channels list in starred tab

--- src/Components/MetaPanel/index.js
+++ src/Components/MetaPanel/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+import { Segment, Accordion, Header, Icon } from "semantic-ui-react";
 
 class MetaPanel extends Component {
-    state = {};
+    state = {
+        activeIndex: 0
+    };
+
+    setActiveIndex = (event, titleProps) => {
+        const { index } = titleProps;
+        const { activeIndex } = this.state;
+        const newIndex = activeIndex === index ? -1 : index;
+        this.setState({ activeIndex: newIndex });
+    };
+
     render() {
-        return <div>Meta Panel</div>;
+        const { activeIndex } = this.state;
+        return (
+            <Segment>
+                <Header as="h3" attached="top">
+                    About # Channel
+                </Header>
+                <Accordion styled attached="true">
+                    <Accordion.Title
+                        active={activeIndex === 0}
+                        index={0}
+                        onClick={this.setActiveIndex}
+                    ></Accordion.Title>
+                </Accordion>
+            </Segment>
+        );
     }
 }
 
--- src/Components/SidePanel/Starred.js
+++ src/Components/SidePanel/Starred.js
@@ -2,11 +2,47 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { setCurrentChannel, setPrivateChannel } from "../../Containers/Root/actions";
 import { Menu, Icon } from "semantic-ui-react";
+import firebase from "../../firebase";
 
 class Starred extends Component {
     state = {
+        user: this.props.currentUser,
+        usersRef: firebase.database().ref("users"),
         activeChannel: "",
         starredChannels: []
+    };
+
+    componentDidMount() {
+        this.state.user && this.addListeners(this.state.user.uid);
+    }
+
+    addListeners = userId => {
+        this.state.usersRef
+            .child(userId)
+            .child("starred")
+            .on("child_added", snap => {
+                const starredChannel = {
+                    id: snap.key,
+                    ...snap.val()
+                };
+                this.setState({
+                    starredChannels: [...this.state.starredChannels, starredChannel]
+                });
+            });
+
+        this.state.usersRef
+            .child(userId)
+            .child("starred")
+            .on("child_removed", snap => {
+                const channelToRemove = {
+                    id: snap.key,
+                    ...snap.val()
+                };
+                const filteredChannels = this.state.starredChannels.filter(channel => {
+                    return channel.id !== channelToRemove.id;
+                });
+                this.setState({ starredChannels: filteredChannels });
+            });
     };
 
     displayChannels = starredChannels =>
